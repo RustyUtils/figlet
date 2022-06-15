88e62eb50619a9451b80d901e2b8ff263d2ddfac
Thamognya Kodi <contact@thamognya.com>
2022-06-15 17:07:28 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ pub fn main() {
         println!("Please enter only 1 or 2 arguments no more or less. You do not have an arg.");
         println!("Usage: ./biglet -h or ./biglet _word_here_");
         println!("Program made by Thamognya Kodi AGPL3.0-or-later");
+        println!("Source Code: https://git.thamognya.com/Thamognya/BigLet or https://github.com/Thamognya/BigLet");
     }
     // or more than 2
     /* remove this as after -h or -_identifier_ there can be many words that need to be big let
@@ -35,18 +36,28 @@ pub fn main() {
         // asign variables for the args
         // let arg1 = &args[1]; // arg 1 can contain -h or etc or just the text to increase. We can just use args[1]
         // check for -h or any other modifiers before just 
-        if args[1].eq("-h") // if arg1 is equal to -h
+        if args[1].eq("-h") || args[1].eq("--help") // if arg1 is equal to -h
         {
-            println!("Testing this first");
+            println!("USAGE: \n\t./biglet [options(optional)] [args]");
+            println!("ARGS: \n\t<args>...");
+            println!("OPTIONS: ");
+            println!("\t-h or --help: Help sheet");
+            println!("\t-i or --italic: italic text");
+            println!("\t-b or --bold: bold text");
+            println!("EXAMPLES: ");
+            println!("\tbiglet Hello \n\tbiglet -i Hello");
+            println!("-----------------------");
+            println!("Program made by Thamognya Kodi AGPL3.0-or-later");
+            println!("Source Code: https://git.thamognya.com/Thamognya/BigLet or https://github.com/Thamognya/BigLet");
         }
-        else if args[1].eq("-i")
+        else if args[1].eq("-i") || args[1].eq("--italic")
         {
             // will make this italic
             arg_string = args[2..].join(" "); // combine all strings after ./biglet -i to make one complete string of arg
             println!("{}", arg_string);
             println!("Work in progress");
         }
-        else if args[1].eq("-b")
+        else if args[1].eq("-b") || args[1].eq("--bold")
         {
             // make this bolded
             arg_string = args[2..].join(" "); // combine all strings after ./biglet -b to make one complete string of arg
